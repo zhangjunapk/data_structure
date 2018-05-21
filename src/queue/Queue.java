20d162ecda0c8b8aecf1f9be91cb4826326af8ba
张君 <1028191676@qq.com>
2018-05-21 23:55:15 +0800

    queue理解错误(弄成后进先出了)

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -5,7 +5,7 @@ package queue;
  * @author 张君
  */
 public class Queue<T> {
-    private Node<T> first=new Node<>();
+    public Node<T> first=new Node<>();
     private Node<T> last=first;
     private Node<T> cursor=first;
 
@@ -14,9 +14,17 @@ public class Queue<T> {
      * @return
      */
     public Node<T> pull(){
-        Node<T> result=last;
-        if(last.getLast()!=null){
-            last=last.getLast();
+        //保存下结果
+        Node<T> result=first;
+
+        //如果第一个节点有下一个
+        if(first.getNext()!=null){
+            //那就将下一个节点的上一个节点地址消失
+            first.getNext().setLast(null);
+            //然后将第一个节点的连接到下一个
+            first=first.next;
+            //然后设置上一个节点为空(因为这个节点要作为第一个)
+            first.setLast(null);
         }
         return result;
     }
